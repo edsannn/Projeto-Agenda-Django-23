d8d4dd223bc4e95ad993c167f3e6387536601385
edsannn <cmo.edson@gmail.com>
2023-08-04 00:02:14 -0300

    Criando o campo owner usando o model User do Django

--- contact/models.py
+++ contact/models.py
@@ -1,21 +1,25 @@
 from django.db import models
 from django.utils import timezone
+from django.contrib.auth.models import User
 # Create your models here.
-
-# id (primary key - automático)
-# first_name (string), last_name (string), phone (string)
-# email (email), created_date (date), description (text)
-
-# Depois
-# category (foreign key), show (boolean), owner (foreign key)
-# picture (imagem)
 
 
 class Category(models.Model):
+    class Meta:
+        verbose_name = 'Category'
+        verbose_name_plural = 'Categories'
+
     name = models.CharField(max_length=50)
+
+    def __str__(self) -> str:
+        return f'{self.name}'
 
 
 class Contact(models.Model):
+
+    class Meta:
+        verbose_name = 'Contact'
+
     first_name = models.CharField(max_length=50)
     last_name = models.CharField(max_length=50, blank=True)
     phone = models.CharField(max_length=50)
@@ -29,6 +33,13 @@ class Contact(models.Model):
         on_delete=models.SET_NULL,
         blank=True, null=True
     )
+    owner = models.ForeignKey(
+        User,
+        on_delete=models.SET_NULL,
+        blank=True, null=True
+    )
+
+
 
     def __str__(self) -> str:
         return f'{self.first_name} {self.last_name}'
